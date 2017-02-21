417176ae04e0766dd784c8cbdf38e4ab80e5bce0
hadware <hadwarez@gmail.com>
2017-02-21 21:40:57 +0100

    Added mingus python notebook and a bit of refactor

--- beats_n_melody.py
+++ beats_n_melody.py
@@ -1,49 +1,26 @@
-from typing import List
-import json
-from os import path
 import logging
-import numpy as np
 from io import BytesIO
+
+import numpy as np
 from scipy.io import wavfile
-
 from voxpopuli import Voice, PhonemeList
-from voxpopuli.phonemes import Frenchphonemes, Phoneme
+from voxpopuli.phonemes import Phoneme
 
+from cock_music_smart_music import get_random_progression_freqs
 from tools.audio import to_f32_16k, to_wav_bytes, mix_tracks
-from tools.melody import get_harmonies
+from tools.melody import get_trinote_progression_freqs
 from tools.player import AudioPlayer
 
-TEMPO = 100
+TEMPO = 50
 BEATS_PER_MEASURE = 4
-CHORDS_PROGRESSION = ["Am0", "Am0", "F0", "Em0"]
-LOOPS_COUNT = 10
+CHORDS_PROGRESSION = ["Am2", "Dm2", "Am2", "E2"]
+LOOPS_COUNT = 40
 
 BEATS_TRACK_FILE = None
-BEATS_TRACK_FILE = "tools/data/beats/sample_6.wav"
-BEATS_TRACK_BEAT_COUNT = 8
+BEATS_TRACK_FILE = "tools/data/beats/tight_beat.wav"
+BEATS_TRACK_BEAT_COUNT = 16
 
-pitch_filepath = path.join(path.dirname(path.realpath(__file__)), "tools/data/pitches.json")
-with open(pitch_filepath) as pitch_file:
-    pitches = json.load(pitch_file)
 
-def get_chord_harmonies(chord_str : str) -> List[int]:
-    octave = int(chord_str[-1])
-    chord = chord_str[:-1]
-    if chord.endswith("m"):
-        note, chord_type = chord.strip("m"), "minor"
-    else:
-        note, chord_type = chord, "major"
-    return get_harmonies(pitches[note + str(octave)], chord_type)
-
-def get_progression_freqs(progression : List[str]) -> List[float]:
-    track_frequencies = []
-    blinker = False
-    for chord in progression:
-        chords_freqs = get_chord_harmonies(chord)
-        for i in range(BEATS_PER_MEASURE):
-            track_frequencies.append(chords_freqs[2] if blinker else chords_freqs[0])
-            blinker = not blinker
-    return track_frequencies
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
@@ -54,29 +31,34 @@ if __name__ == "__main__":
         rate, beats_track = wavfile.read(BEATS_TRACK_FILE)
         beat_duration = len(beats_track) / (rate * BEATS_TRACK_BEAT_COUNT)
         beats_track_looped = np.tile(beats_track,
-                                     LOOPS_COUNT * BEATS_PER_MEASURE * LEN(CHORDS_PROGRESSION // BEATS_TRACK_BEAT_COUNT))
+                                     LOOPS_COUNT * BEATS_PER_MEASURE * len(CHORDS_PROGRESSION) // BEATS_TRACK_BEAT_COUNT)
 
     logging.info("Beat time : %dms" % (beat_duration * 1000))
     logging.info("Measure time : %dms" % (beat_duration * 1000 * 4))
 
-    prog_freqs = get_progression_freqs(CHORDS_PROGRESSION)
-    logging.info("First freq progression: \n %s" % (str(prog_freqs)))
-    track_freqs = prog_freqs * LOOPS_COUNT
+    # prog_freqs = get_trinote_progression_freqs(CHORDS_PROGRESSION, BEATS_PER_MEASURE)
+    # logging.info("First freq progression: \n %s" % (str(prog_freqs)))
+    track_freqs = []
+    for _ in range(LOOPS_COUNT):
+        track_freqs += get_trinote_progression_freqs(CHORDS_PROGRESSION, BEATS_PER_MEASURE)
+        # track_freqs += get_random_progression_freqs(CHORDS_PROGRESSION)
 
     progression_phonems = PhonemeList([])
     for freq in track_freqs:
         progression_phonems.append(
-            Phoneme("a", int(beat_duration * 1000), [(0,freq), (100,freq)])
+            Phoneme("o", int(beat_duration * 1000), [(0,freq), (100,freq)])
         )
     logging.info("Rendering audio")
 
-    voice = Voice(lang="fr", voice_id=2)
+    voice = Voice(lang="fr", voice_id=4)
     wav = voice.to_audio(progression_phonems)
     if BEATS_TRACK_FILE is not None:
         rate, wave_array = to_f32_16k(wav)
-        mixed_tracks = mix_tracks(beats_track_looped * 0.6, wave_array * 0.2, align="left")
+        mixed_tracks = mix_tracks(beats_track_looped * 0.5, wave_array * 0.7, align="left")
         wav = to_wav_bytes(mixed_tracks, 16000)
     player = AudioPlayer()
+    with open("/tmp/gros_beat.wav", "wb") as wavfile:
+        wavfile.write(wav)
     player.set_file(BytesIO(wav))
     player.play()
     player.close()
--- tools/melody.py
+++ tools/melody.py
@@ -1,6 +1,9 @@
 
 """Tools for melodies used in phonems"""
 from typing import List
+import random
+from os import path
+import json
 
 au_clair_de_la_lune = ["C4"] * 3 + ["D4", "E4", "C4", "D4", "E4", "D4"]
 
@@ -29,3 +32,60 @@ def get_freqs(fundamental : int, ratios : List[int]):
 
 def get_harmonies(note_pitch, chord_type="major") -> List[int]:
     return get_freqs(note_pitch, chords_ratios[chord_type])
+
+pitch_filepath = path.join(path.dirname(path.realpath(__file__)), "data/pitches.json")
+with open(pitch_filepath) as pitch_file:
+    pitches = json.load(pitch_file)
+
+def get_chord_harmonies(chord_str : str) -> List[int]:
+    octave = int(chord_str[-1])
+    chord = chord_str[:-1]
+    if chord.endswith("m"):
+        note, chord_type = chord.strip("m"), "minor"
+    else:
+        note, chord_type = chord, "major"
+    return get_harmonies(pitches[note + str(octave)], chord_type)
+
+def get_progression_freqs(progression : List[str], beats_per_measure) -> List[float]:
+    track_frequencies = []
+    blinker = False
+    for chord in progression:
+        chords_freqs = get_chord_harmonies(chord)
+        for i in range(beats_per_measure):
+            track_frequencies.append(chords_freqs[2] if blinker else chords_freqs[0])
+            blinker = not blinker
+    return track_frequencies
+
+def get_trinote_progression_freqs(progression : List[str], beats_per_measure) -> List[float]:
+    track_frequencies = []
+    freqs_index = [0, 2, 0, 1]
+    for chord in progression:
+        chords_freqs = get_chord_harmonies(chord)
+        for i in range(beats_per_measure):
+            track_frequencies.append(chords_freqs[freqs_index[i]])
+    return track_frequencies
+
+def get_random_progression_freqs(progression : List[str], beats_per_measure) -> List[float]:
+    track_frequencies = []
+    freqs_index = [0, 2, 0, 1]
+    for chord in progression:
+        chords_freqs = get_chord_harmonies(chord)
+        for i in range(beats_per_measure):
+            track_frequencies.append(chords_freqs[random.choice(freqs_index)])
+    return track_frequencies
+
+
+class AudioTrack:
+    pass
+
+
+class MelodicSynthTrack(AudioTrack):
+
+    def __init__(self, beats_per_measures, chords_progression):
+        pass
+
+
+class RawAudioSynthTrack(AudioTrack):
+
+    def __init__(self, audio_file):
+        pass
